446d5aad1404915e3316e9d4cac202299d10174f
Akshat <22b0690@iitb.ac.in>
2023-08-26 14:34:02 +0530

    header toggle Corrected

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,3 +1,46 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+const Header = () => {
+  const [collapsed, setCollapsed] = useState(true);
+
+  const toggleNavbar = () => setCollapsed(!collapsed);
+
+  return (
+    <nav align="center" className="navbar navbar-expand-lg navbar-dark bg-primary custom-navbar">
+      <Link className="navbar-brand ml-3"  style={{marginLeft:"15px"}}to="/">AxeAcads</Link>
+      <button className="navbar-toggler" type="button" onClick={toggleNavbar} aria-controls="navbarNav" aria-expanded={!collapsed} aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className={`${collapsed ? 'collapse' : ''} navbar-collapse`} id="navbarNav">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link className="nav-link" align="center" to="/">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link"  align="center" to="/about">About Me</Link>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" align="center" href="https://akshat1423.github.io/diary/">Note Making</a>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link"  align="center"to="/help">Need any Help? Doubt? Query?</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" align="center" to="/suggestion">Suggestion? Experience? </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
+
+
+
 // import React, { useState } from 'react';
 // import { Link } from 'react-router-dom';
 
@@ -25,37 +68,3 @@
 // };
 
 // export default Header;
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-const Header = () => {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary custom-navbar"  >
-      <Link className="navbar-brand ml-3" style={{ marginLeft: '30px' }} to="/">AxeAcads</Link>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-            <Link className="nav-link" to="/">Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/about">About Me</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="https://akshat1423.github.io/diary/">Note Making</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/help">Need any Help? Doubt? Query?</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/suggestion">Suggestion? Experience? </Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
-};
-
-export default Header;
