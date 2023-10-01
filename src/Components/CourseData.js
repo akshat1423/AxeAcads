48579784b8663e5b69daf1199ec61953b5f7d0cf
akshat1423 <125379408+akshat1423@users.noreply.github.com>
2023-10-01 21:43:10 +0530

    Update CourseData.js

--- src/Components/CourseData.js
+++ src/Components/CourseData.js
@@ -1,17 +1,29 @@
-export  const courseData = [
-    {name: 'Calculus', 
-    Notes:"https://drive.google.com/drive/u/1/folders/1-kj9AiiBo_gRXPutL0CBbIzLCR5gllMo",
-    Slides:"https://drive.google.com/drive/u/1/folders/1rnlii19uPJaT-VaCFusWoik4afbAaVPC",
-    Exam_Papers:"https://drive.google.com/drive/u/1/folders/1ddH3FKqB4Y8WpPQCPPPc2OUFRkMG9uQO", 
-    tutorials:"https://drive.google.com/drive/u/1/folders/1t6oW2D_oDWCw94Sdl4tHziyG9IiFTuvT"},
-
-    {name: 'Organic Chemistry CH111', 
-    Slides:"https://drive.google.com/drive/u/1/folders/1oMFHbw3ikM5YRLYVAkWaIEDUUsWkm3M8",
-    Exam_Papers:"https://drive.google.com/drive/u/1/folders/1JVv1D5O7xgie3HlVfY8byGnQ-6SGWmvf", 
-    tutorials:"https://drive.google.com/drive/u/1/folders/1K1McWYZlXM2DZ4wzJhEAS8yOSjJDLrQO"},
-    {name: 'C++ Theory', 
-    Slides:"https://www.cse.iitb.ac.in/~ajitvr/CS101_Fall2022/",
-    // Exam_Papers:"https://drive.google.com/drive/u/1/folders/1JVv1D5O7xgie3HlVfY8byGnQ-6SGWmvf", 
-    // tutorials:"https://drive.google.com/drive/u/1/folders/1K1McWYZlXM2DZ4wzJhEAS8yOSjJDLrQO"
-}
-]
+export const courseData = [
+    {
+        name: 'Calculus',
+        Notes: "https://drive.google.com/drive/u/1/folders/1-kj9AiiBo_gRXPutL0CBbIzLCR5gllMo",
+        Slides: "https://drive.google.com/drive/u/1/folders/1rnlii19uPJaT-VaCFusWoik4afbAaVPC",
+        Exam_Papers: "https://drive.google.com/drive/u/1/folders/1ddH3FKqB4Y8WpPQCPPPc2OUFRkMG9uQO",
+        tutorials: "https://drive.google.com/drive/u/1/folders/1t6oW2D_oDWCw94Sdl4tHziyG9IiFTuvT"
+    },
+    {
+        name: 'Organic Chemistry CH111',
+        Slides: "https://drive.google.com/drive/u/1/folders/1oMFHbw3ikM5YRLYVAkWaIEDUUsWkm3M8",
+        Exam_Papers: "https://drive.google.com/drive/u/1/folders/1JVv1D5O7xgie3HlVfY8byGnQ-6SGWmvf",
+        tutorials: "https://drive.google.com/drive/u/1/folders/1K1McWYZlXM2DZ4wzJhEAS8yOSjJDLrQO"
+    },
+    {
+        name: 'C++ Theory',
+        Slides: "https://www.cse.iitb.ac.in/~ajitvr/CS101_Fall2022/",
+    },
+    {name: 'Course 1', Slides: "http://example.com/course1/slides", Exam_Papers: "http://example.com/course1/exams"},
+    {name: 'Course 2', Slides: "http://example.com/course2/slides", Exam_Papers: "http://example.com/course2/exams"},
+    {name: 'Course 3', Slides: "http://example.com/course3/slides", Exam_Papers: "http://example.com/course3/exams"},
+    {name: 'Course 4', Slides: "http://example.com/course4/slides", Exam_Papers: "http://example.com/course4/exams"},
+    {name: 'Course 5', Slides: "http://example.com/course5/slides", Exam_Papers: "http://example.com/course5/exams"},
+    {name: 'Course 6', Slides: "http://example.com/course6/slides", Exam_Papers: "http://example.com/course6/exams"},
+    {name: 'Course 7', Slides: "http://example.com/course7/slides", Exam_Papers: "http://example.com/course7/exams"},
+    {name: 'Course 8', Slides: "http://example.com/course8/slides", Exam_Papers: "http://example.com/course8/exams"},
+    {name: 'Course 9', Slides: "http://example.com/course9/slides", Exam_Papers: "http://example.com/course9/exams"},
+    {name: 'Course 10', Slides: "http://example.com/course10/slides", Exam_Papers: "http://example.com/course10/exams"}
+];
